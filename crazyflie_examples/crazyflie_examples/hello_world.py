a5af2112780080fc8d4c5e7f616aef9549f41815
K4IN1 <sujianzhang76@gmail.com>
2024-07-04 20:12:44 +0800

    Unabled motion_capture enable pinocchio sim

--- crazyflie_examples/crazyflie_examples/hello_world.py
+++ crazyflie_examples/crazyflie_examples/hello_world.py
@@ -13,22 +13,22 @@ def main():
     cf1 = swarm.allcfs.crazyfliesByName['cf1']
     cf3 = swarm.allcfs.crazyfliesByName['cf3']
 
-    cf1.setParam('led.bitmask',255)
+    # cf1.setParam('led.bitmask',255)
     cf1.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
     timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
-    cf1.goTo([1.0,1.0,1.0],0,3)
-    timeHelper.sleep(HOVER_DURATION)
+    # cf1.goTo([1.0,1.0,1.0],0,3)
+    # timeHelper.sleep(HOVER_DURATION)
     cf1.land(targetHeight=0.04, duration=2.5)
     timeHelper.sleep(TAKEOFF_DURATION)
-    cf1.setParam('led.bitmask',0)
+    # cf1.setParam('led.bitmask',0)
     
     # cf3.setParam('led.bitmask',255)
-    # cf3.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
-    # timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
+    cf3.takeoff(targetHeight=1.0, duration=TAKEOFF_DURATION)
+    timeHelper.sleep(TAKEOFF_DURATION + HOVER_DURATION)
     # cf3.goTo([1.0,1.0,1.0],0,3)
     # timeHelper.sleep(HOVER_DURATION)
-    # cf3.land(targetHeight=0.04, duration=2.5)
-    # timeHelper.sleep(TAKEOFF_DURATION)
+    cf3.land(targetHeight=0.04, duration=2.5)
+    timeHelper.sleep(TAKEOFF_DURATION)
     # cf3.setParam('led.bitmask',0)
 
 if __name__ == '__main__':
